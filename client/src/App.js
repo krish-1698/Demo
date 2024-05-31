a2d9c4b6c74488cb01245bfa46b7e507319784dc
Kishan <kparamanayagam@gmail.com>
2024-05-31 20:18:51 +0530

    Created the UIs for login, home pages

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Login from './pages/login'
+import Home from './pages/home';
+import './App.css';
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [email, setEmail] = useState('');
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
